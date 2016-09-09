171a1b6afda5e16a17f7e00e56c875c733136209
Samu Aronen <samu.aronen@cs.tamk.fi>
2016-09-09 11:23:15 +0300

    Initial Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Runner { 
+public class CameraFollow : MonoBehaviour {
+        [SerializeField] private Transform _player;
+
+	// Update is called once per frame
+	void Update ()
+    {
+            transform.position = new Vector3(_player.position.x + 6,0,transform.position.z);
+	}
+}
+}
